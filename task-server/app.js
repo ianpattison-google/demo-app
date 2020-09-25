11c65833f2ebd7cd3a2ff5b0df638491c9eee41c
Ian Pattison <ianpattison@google.com>
2020-09-25 16:58:48 +0100

    switch to sqlite3 for storage

--- task-server/app.js
+++ task-server/app.js
@@ -2,22 +2,55 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// local storage
-const todos = [];
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory');
+// const sequelize = new Sequelize('react-tasks', 'root', 'password', {
+//     dialect: 'mysql',
+//     host: '/cloudsql/ianpattison-react-tasks:europe-west1:react-tasks',
+//     dialectOptions: {
+//         socketPath: '/cloudsql/ianpattison-react-tasks:europe-west1:react-tasks'
+//     }
+// });
+
+// define the ORM model
+const Todo = sequelize.define('Todo', {
+    id:          { type: DataTypes.INTEGER, allowNull: false, primaryKey: true},
+    text:        { type: DataTypes.STRING,  allowNull: false},
+    isCompleted: { type: DataTypes.BOOLEAN, allowNull: false}
+});
+
+// set up test data if required
+// check the size of the collection
+Todo.sync()
+.then(p => Todo.count())
+.then(count => {
+    console.log(count);
+    if (count == 0) {
+        Todo.create({ id:1, text: "Learn React", isCompleted: false });
+        Todo.create({ id:2, text: "Have lunch", isCompleted: false });
+        Todo.create({ id:3, text: "Write React app", isCompleted: false });
+    }
+})
 
 app.use(cors());
 app.use(express.json());
 
 // GET a todo item by its ID value
 app.get('/todos/:id', (req, res) => {
-    const todo = todos.find(x => x.id == parseInt(req.params.id));
-    if (!todo) { return res.sendStatus(404); }
-    res.json(todo);
+    Todo.findByPk(parseInt(req.params.id))
+    .then(todo => { res.json(todo) })
 })
 
 // GET all todo items
 app.get('/todos', (req, res) => {
-    res.json(todos);
+    const todos = [];
+    Todo.findAll()
+    .then(result => {
+        result.forEach(todo => {
+            todos.push(todo);
+        })
+        res.json(todos);
+    })
 })
 
 // GET default behaviour
@@ -32,14 +65,10 @@ app.post('/todos', (req, res) => {
     // check for any required fields
     if (!id || !text) { return res.status(400).send('One or more required fields are missing'); }
 
-    // check for duplicate ID
-    const todo = todos.find(x => x.id == id);
-    if (todo) { return res.sendStatus(404); }
-
     // add the new todo item
     const newTodo = { id, text, isCompleted };
-    todos.push(newTodo);
-    res.sendStatus(200); 
+    Todo.create(newTodo)
+    .then(p => res.sendStatus(200));
 })
 
 // PUT an item - updates existing
@@ -49,22 +78,19 @@ app.put('/todos/:id', (req, res) => {
     // check for any required fields
     if (!id || !text) { return res.status(400).send('One or more required fields are missing'); }
 
-    // check that the item to be updated actually exists
-    const index = todos.findIndex(x => x.id == parseInt(req.params.id));
-    if (index == -1) { return res.sendStatus(404); }
-
     // update the todo item
     const updatedTodo = { id, text, isCompleted };
-    todos[index] = updatedTodo;
-    res.sendStatus(200);
+    Todo.findByPk(parseInt(req.params.id))
+    .then(todo => todo.update(updatedTodo))
+    .then(p => res.sendStatus(200));
+
 })
 
 // DELETE an item
 app.delete('/todos/:id', (req, res) => {
-    const index = todos.findIndex(x => x.id == parseInt(req.params.id));
-    if (index == -1) { return res.sendStatus(404); }
-    todos.splice(index, 1);
-    res.sendStatus(200); 
+    Todo.findByPk(parseInt(req.params.id))
+    .then(todo => todo.destroy())
+    .then(p => res.sendStatus(200));
 })
 
 // we need to set up an app.js and server.js in this way so that we can test it with Jest
